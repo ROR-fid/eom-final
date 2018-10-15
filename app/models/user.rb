42895c95a64254f29d756db5f4e71a6006ee2b45
alulu26 <ludwig.astrid@googlemail.com>
2018-10-15 22:13:26 +0200

    Login included

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180720201601) do
+ActiveRecord::Schema.define(version: 20180928111742) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -33,8 +33,10 @@ ActiveRecord::Schema.define(version: 20180720201601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "votes", force: :cascade do |t|
